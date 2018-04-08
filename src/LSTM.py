31eb9e1ce82e465699271742273019a6c1767d69
Abhinav-Reddy <abhinavpodduturi@ufl.edu>
2018-04-08 02:03:51 +0000

    Best results till now

--- src/LSTM.py
+++ src/LSTM.py
@@ -34,38 +34,41 @@ RESULTS_PATH = "../results/"
 #LOADS TRAINING AND TEST SET
 train_df = pd.read_csv(TRAIN_CSV)
 test_df = pd.read_csv(TEST_CSV)
-seqLength = 14
+seqLength = 13
 dict = {}
-dict['A'] = 1
-dict['C'] = 2
-dict['G'] = 3
-dict['T'] = 4
+dict['A'] = 0
+dict['C'] = 1
+dict['G'] = 2
+dict['T'] = 3
 
 
-embedding_dim = 4
-vocabulary = 4
-embeddings = 1 * np.random.randn(vocabulary + 1, embedding_dim)
-embeddings[0] = 0
-embeddings[1] = np.array([1,0,0,0])
-embeddings[2] = np.array([0,1,0,0])
-embeddings[3] = np.array([0,0,1,0])
-embeddings[4] = np.array([0,0,0,1])
-
+embedding_dim = 16
+vocabulary = 16
+embeddings = np.zeros((vocabulary + 1, embedding_dim))
+embeddings.fill(0)
+for i in range(vocabulary):
+	embeddings[i+1][i] = 1
+embeddings[0][0] = 0
 
 for dataTuple in [train_df, test_df]:
 	for index, row in dataTuple.iterrows():
 		numVector = []
-		for ch in row['sequence']:
-			numVector.append(dict[ch])
+		curStr = row['sequence']
+		for i in range(len(curStr)-1):
+			curVal = 0
+			#if (curStr[i] != curStr[i+1]):
+			for j in range(2):
+				curVal = (4**j)*curVal + dict[curStr[i+j]]
+			numVector.append(curVal+1)
 		dataTuple.set_value(index, 'sequence', numVector)
 
-validation_size = 400
+validation_size = 100
 xTrain, xValidation, yTrain, yValidation = train_test_split(train_df['sequence'], train_df['label'], test_size=validation_size)
 
 
 xValidation = np.array(xValidation.tolist())
 xTrain = np.array(xTrain.tolist())
-
+print(xValidation)
 # Model variables
 n_hidden = 16
 gradientClippingNorm = 1.25
